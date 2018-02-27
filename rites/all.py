435704e660e78ec767eade1c3e5e2cd112872e4e
tonyfast <tony.fast@gmail.com>
2018-02-27 13:32:30 -0500

    introduce conventions for yaml, grapviz, and iframe

--- rites/all.py
+++ rites/all.py
@@ -1,3 +1,7 @@
+from . import conventions, markdown, template, testing
+
 def load_ipython_extension(ip):
-    from . import markdown, template, testing
-    [module.load_ipython_extension(ip)for module in (markdown, testing, template)]
+    for module in (conventions, markdown, testing, template):
+        if module.__complete__ is not True:
+            raise module.__complete__
+        module.load_ipython_extension(ip)
