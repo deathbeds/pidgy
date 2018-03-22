be79f296155cb9f72895204e5736eb1a27694add
tonyfast <tony.fast@gmail.com>
2018-03-21 21:30:29 -0400

    changes that assisted in the ten presentation

--- rites/__init__.py
+++ rites/__init__.py
@@ -1,5 +1,4 @@
 from .rites import load_ipython_extension, unload_ipython_extension 
 load_ipython_extension()
-
-from . import markdown, testing, template
-from .widgets import manager
+from .sugar import load_ipython_extension
+load_ipython_extension()
