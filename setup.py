13c5cae8b7dc0a3668f7084fa968fa36d1793aa7
tonyfast <tony.fast@gmail.com>
2018-04-07 19:20:29 -0400

    Add dependency links

--- setup.py
+++ setup.py
@@ -4,7 +4,12 @@ __import__('setuptools').setup(
     author="deathbes", author_email="tony.fast@gmail.com",
     description="Macros for computable essays.", 
     license="BSD-3-Clause",
-    install_requires=[],
+    install_requires=['importnb', 'sweet'],
     include_package_data=True,
     packages=['rites'],
+    dependency_links=[
+        'https://github.com/deathbeds/importnb/archive/master.tar.gz#egg=importnb-0.0.1',
+        'https://github.com/deathbeds/sweet/archive/master.tar.gz#egg=sweet-0.0.1'
+    ]
+    
 )
