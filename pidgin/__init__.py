7621b8191e03da43ff8c6e53fd105372d298b8a4
Tony Fast <tony.fast@gmail.com>
2018-07-12 21:51:53 -0400

    Changes to pidgin using the newest versions of importnb

--- pidgin/__init__.py
+++ pidgin/__init__.py
@@ -1,40 +1,14 @@
 
 # coding: utf-8
 
-# In[4]:
+# In[1]:
 
 
 with __import__('importnb').Notebook():
-    try:
-        from . import config, conventions, markdown, recall, template
-    except:
-        import config, conventions, markdown, recall, template
-
-
-# In[ ]:
-
-
+    from .markdown import MarkdownImporter
+    from .template import Jinja2Importer, Jinja2MarkdownImporter
+        
 def load_ipython_extension(ip):
-    get_ipython().run_line_magic('reload_ext', 'importnb')
-    from pidgin.config import alias
-    if __name__ == '__main__':
-        get_ipython().run_line_magic('reload_ext', 'config')
-    else:
-        get_ipython().run_line_magic('reload_ext', 'pidgin.config')
-
-
-# In[ ]:
-
-
-def unload_ipython_extension(ip):
-    from IPython.core.inputsplitter import IPythonInputSplitter
-    ip.input_transformer_manager = IPythonInputSplitter()
-
-
-# In[ ]:
-
-
-if __name__ == '__main__':
-    get_ipython().system('jupyter nbconvert --to python __init__.ipynb')
-#         load_ipython_extension(__import__('IPython').get_ipython())
-
+    MarkdownImporter(display=True).__enter__()
+    Jinja2Importer().__enter__()
+    Jinja2MarkdownImporter(display=True).__enter__()
