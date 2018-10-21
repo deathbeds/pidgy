8d301a6ba0152a7ed894fcb0a07823b7bcf924bd
Tony Fast <tony.fast@gmail.com>
2018-10-21 14:07:18 -0400

    Major refactoring.

--- pidgin/__init__.py
+++ pidgin/__init__.py
@@ -5,18 +5,20 @@ with __import__('importnb').Notebook():
     from . import magics
     from . import shell
     from . import kernel
-    from . import stringdisplays
+    from . import macros
+    
+_ipython_display_ = shell._ipython_display_
 
 def load_ipython_extension(ip):
     MarkdownImporter().__enter__()
     # Jinja2Importer().__enter__()
     # Jinja2MarkdownImporter().__enter__()
-    for module in (magics, shell, kernel, stringdisplays):
+    for module in (magics, shell, kernel, macros):
         module.load_ipython_extension(ip)
         
 def unload_ipython_extension(ip):
     MarkdownImporter().__exit__()
     # Jinja2Importer().__enter__()
     # Jinja2MarkdownImporter().__enter__()
-    for module in (magics, shell, kernel, stringdisplays):
+    for module in (magics, shell, kernel, macros):
         module.unload_ipython_extension(ip)
