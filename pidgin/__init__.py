d5da848d111d3a9975867307907bba8dbb29794a
Tony Fast <tony.fast@gmail.com>
2018-11-19 10:36:40 -0500

    Rename formatter to display

--- pidgin/__init__.py
+++ pidgin/__init__.py
@@ -4,24 +4,18 @@ from .loader import PidginImporter
 with __import__('importnb').Notebook():
     from . import shell
     from . import kernel
-    from . import formatter
+    from . import display
 
 import IPython
 
 def load_ipython_extension(ip=None):
     ip = ip or IPython.get_ipython()
-    for module in (shell, formatter): module.load_ipython_extension(ip)
-    try:
-        kernel.load_ipython_extension(ip)
-    except AttributeError: "There is no kernel to replace"
+    for module in (shell, kernel, display): module.load_ipython_extension(ip)
 
 load = load_ipython_extension
 
 def unload_ipython_extension(ip):
-    for module in (shell, formatter):
+    for module in (shell, display, kernel):
         module.unload_ipython_extension(ip)
-    try:
-        kernel.unload_ipython_extension(ip)
-    except: "There is no kernel to replace"
 
 unload = unload_ipython_extension
