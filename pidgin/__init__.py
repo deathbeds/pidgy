5f0138ab2f03e9afbf8c1cb56c813a8acde2d289
Tony Fast <tony.fast@gmail.com>
2018-11-20 17:20:39 -0500

    Update __init__.py
    Add tangle to the initialization.

--- pidgin/__init__.py
+++ pidgin/__init__.py
@@ -1,6 +1,7 @@
 from ._version import *
 with __import__('importnb').Notebook():
     from . import shell
+    from . import tangle
     from . import kernel
     from . import display
     from .loader import PidginImporter
@@ -10,12 +11,12 @@ import IPython
 
 def load_ipython_extension(ip=None):
     ip = ip or IPython.get_ipython()
-    for module in (shell, kernel, display): module.load_ipython_extension(ip)
+    for module in (shell, kernel, display, tangle): module.load_ipython_extension(ip)
 
 load = load_ipython_extension
 
 def unload_ipython_extension(ip):
-    for module in (shell, display, kernel):
+    for module in (shell, display, kernel, tangle):
         module.unload_ipython_extension(ip)
 
 unload = unload_ipython_extension
