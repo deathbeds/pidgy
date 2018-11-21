386d1f67c39b852572b35d3a0a9dad1e41b3fa3c
Tony Fast <tony.fast@gmail.com>
2018-11-21 08:11:33 -0500

    Update __init__.py
    Rename modules in init

--- pidgin/__init__.py
+++ pidgin/__init__.py
@@ -1,8 +1,8 @@
 from ._version import *
 with __import__('importnb').Notebook():
-    from . import shell
+    from . import post_run_cell
     from . import tangle
-    from . import kernel
+    from . import inspector
     from . import display
     from .loader import PidginImporter
 
@@ -11,12 +11,12 @@ import IPython
 
 def load_ipython_extension(ip=None):
     ip = ip or IPython.get_ipython()
-    for module in (shell, kernel, display, tangle): module.load_ipython_extension(ip)
+    for module in (post_run_cell, inspector, display, tangle): module.load_ipython_extension(ip)
 
 load = load_ipython_extension
 
 def unload_ipython_extension(ip):
-    for module in (shell, display, kernel, tangle):
+    for module in (post_run_cell, inspector, display, tangle):
         module.unload_ipython_extension(ip)
 
 unload = unload_ipython_extension
