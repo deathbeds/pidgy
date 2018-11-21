b6f56ec774af02079524d239c504b0f16f02ccaf
Tony Fast <tony.fast@gmail.com>
2018-11-21 10:47:23 -0500

    Update setup.py
    Add dataclasses as dep

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup_args = dict(
     setup_requires=['pytest-runner'],
     tests_require=['pytest', "hypothesis"],
     install_requires=[
-        "nbconvert", "importnb", "IPython>7"
+        "nbconvert", "importnb", "IPython>7", 'dataclasses'
     ],
     extras_require={
         'graphviz':  ["graphviz"],
