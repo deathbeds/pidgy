5bddc0b2ab73afefc7fea8c863b4807414985bba
tonyfast <tony.fast@gmail.com>
2019-03-05 18:28:24 -0500

    squash

--- src/pidgin/specifications/__init__.py
+++ src/pidgin/specifications/__init__.py
@@ -1,8 +1,8 @@
 with __import__('importnb').Notebook():  
     from .. import tangle
-    from . import transform_cell, emojis_, yaml_, json, transform_ast
-
+    from . import transform_cell
 with tangle.Pidgin():
     from .. import shell
+    from . import emojis, yaml_, json, transform_ast
     from . import  testing, markdown, template
     
