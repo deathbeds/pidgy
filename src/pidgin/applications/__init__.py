3f90f56f4c6b83428a0d2d9ed3bbc4ae1e1cda5f
tonyfast <tony.fast@gmail.com>
2019-03-07 14:10:28 -0500

    move tangle doctests to the shell

--- src/pidgin/applications/__init__.py
+++ src/pidgin/applications/__init__.py
@@ -2,6 +2,6 @@ with __import__('importnb').Notebook():
     from .. import tangle
 
 with tangle.Pidgin(): 
-    from . import loaders, do_inspect, do_execute, do_complete, publishing
+    from . import do_inspect, do_execute, do_complete, publishing, loaders
     from .loaders import *
 
