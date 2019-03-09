9b3fe6f4360d4be3dc3dffd2a451839175f86710
tonyfast <tony.fast@gmail.com>
2019-03-08 23:00:49 -0500

    use ruamel

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup_args = dict(
     setup_requires=['pytest-runner'],
     tests_require=['pytest', "hypothesis", 'nbval'],
     install_requires=[
-        "nbconvert", "importnb", "IPython>7", 'dataclasses', "pyyaml", "pyld", "jsonpointer", "jsonschema", "emoji", "webcolors", "attrs>=17.4.0"
+        "nbconvert", "importnb", "IPython>7", 'dataclasses', "ruamel.yaml", "pyld", "jsonpointer", "jsonschema", "emoji", "webcolors", "attrs>=17.4.0"
     ],
     extras_require={
         'graphviz':  ["graphviz"],
