cb07e4574b578852a4cc565a72644710004d6d7f
tonyfast <tony.fast@gmail.com>
2019-03-21 00:26:41 -0400

    Update __init__.py

--- src/pidgin/specification/__init__.py
+++ src/pidgin/specification/__init__.py
@@ -3,6 +3,6 @@ with __import__('importnb').Notebook():
     from . import transform_cell
 with tangle.Pidgin():
     from .. import shell
-    from . import emojis, yaml, json, transform_ast
+    from . import emojis, json, transform_ast
     from . import  testing, markdown, template, colors
     
