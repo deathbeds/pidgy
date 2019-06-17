bc9a13276848dbb4d487d636471683430077ed01
Tony Fast <tony.fast@gmail.com>
2019-06-17 14:38:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup_args = dict(
     setup_requires=['pytest-runner'],
     tests_require=['pytest', "hypothesis", 'nbval'],
     install_requires=[
-        "nbconvert", "importnb", "IPython>7", 'dataclasses', "ruamel.yaml", "pyld", "jsonpointer", "jsonschema", "emoji", "webcolors", "attrs>=17.4.0"
+        "nbconvert", "importnb", "IPython>7", 'dataclasses', "ruamel.yaml", "pyld", "jsonpointer", "jsonschema", "emoji", "htmlmin", "webcolors", "attrs>=17.4.0"
     ],
     include_package_data=True,
     py_modules=['pidgin'],
