302ec59329e2eab7cfb3a8e11a47b9fa46227067
Tony Fast <tony.fast@gmail.com>
2020-02-12 17:38:41 -0500

    favor markdown for master branch readme

--- setup.py
+++ setup.py
@@ -14,8 +14,9 @@ setup_args = dict(
     author_email="tony.fast@gmail.com",
     description="Conventions for writing code in the notebook.",
     long_description=(
-        (here / "README.rst").read_text() + "\n\n"
+        (here / "README.md").read_text() + "\n\n"
     ),
+    long_description_content_type='text/markdown'
     url="https://github.com/deathbeds/pidgy",
     python_requires=">=3.6",
     license="BSD-3-Clause",
