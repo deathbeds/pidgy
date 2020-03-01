ea14b614df82b40504d7d78670b1059359085161
Tony Fast <tony.fast@gmail.com>
2020-03-01 18:51:14 -0500

    load readme lazily

--- docs/__init__.py
+++ docs/__init__.py
@@ -5,3 +5,9 @@ with pidgy.pidgyLoader(lazy=True):
         from . import intro, best_practices
     except:
         import intro, best_practices
+main = __name__ == "__main__"
+with pidgy.pidgyLoader(main=main, lazy=not main):
+    try:
+        from . import readme
+    except:
+        import readme
--- docs/__main__.py
+++
@@ -1,7 +0,0 @@
-import pidgy
-
-with pidgy.pidgyLoader(main=__name__ == "__main__"):
-    try:
-        from . import readme
-    except:
-        import readme
