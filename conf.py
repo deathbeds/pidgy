4f547fc10a759363d94ae6b4d8b3057eabd35801
Tony Fast <tony.fast@gmail.com>
2020-03-09 10:07:19 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,3 +1,10 @@
-from recommonmark.parser import CommonMarkParser
-source_suffix = ['.md']
-extensions = ['recommonmark']
+# import sphinx.writers.html5
+# sphinx.writers.html5.HTML5Translator.visit_pending_xref = lambda *x:...
+# sphinx.writers.html5.HTML5Translator.depart_pending_xref = lambda *x:...
+html_theme = 'classic'
+master_doc = 'index'
+source_suffix = '.rst .md .ipynb'.split()
+extensions = 'recommonmark nbsphinx'.split() 
+
+# Exclude build directory and Jupyter backup files:
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
