b77734f31eddd19deda237acbfb5989003c80f44
Tony Fast <tony.fast@gmail.com>
2020-03-09 13:36:05 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -3,7 +3,6 @@
 # sphinx.writers.html5.HTML5Translator.depart_pending_xref = lambda *x:...
 title = html_title = "pidgy literate computing"
 author = "Tony Fast"
-html_theme = "classic"
 master_doc = "index"
 source_suffix = ".rst .md .ipynb .py".split()
 extensions = "recommonmark nbsphinx sphinx.ext.autodoc sphinx.ext.coverage sphinx.ext.napoleon autoapi.extension sphinx.ext.mathjax sphinx_copybutton".split()
@@ -12,6 +11,8 @@ extensions = "recommonmark nbsphinx sphinx.ext.autodoc sphinx.ext.coverage sphin
 exclude_patterns = ["_build", "*checkpoint*"]
 autoapi_type = "python"
 autoapi_dirs = ["pidgy"]
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
 texinfo_documents = [
     (
         master_doc,
