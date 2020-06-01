9fa8c52ceeb1c994fc5e12ebd68091d4f3817ad6
Tony Fast <tony.fast@gmail.com>
2020-06-01 16:02:13 -0400

    Update base.py

--- pidgy/base.py
+++ pidgy/base.py
@@ -1,5 +1,9 @@
-import pluggy
+import pluggy, traitlets
 
 implementation = pluggy.HookimplMarker("pidgy")
 specification = pluggy.HookspecMarker("pidgy")
 plugin_manager = pluggy.PluginManager("pidgy")
+
+
+class Trait(traitlets.HasTraits):
+    parent = traitlets.Any()
