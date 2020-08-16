05d7a55c8995d9bb419fb4d5afc0ebefec356242
tonyfast <tony.fast@gmail.com>
2020-08-16 10:52:03 -0400

    fix one test

--- pidgy/base.py
+++ pidgy/base.py
@@ -1,4 +1,4 @@
-import traitlets
+import traitlets, IPython
 
 
 class Trait(traitlets.HasTraits):
@@ -13,3 +13,32 @@ class Trait(traitlets.HasTraits):
 
     def toggle(self, object: bool):
         self.enabled = bool(object if object is None else not self.enabled)
+        
+class Display(Trait):
+    body = traitlets.Unicode(allow_none=True)
+    vars = traitlets.Any()
+    _display = traitlets.Any()
+
+    def update(self, **kwargs):
+        if self._display is None: return self.display(**kwargs)
+        self._display.update(self.render(**kwargs))
+
+    def render(self, **kwargs): 
+        if self.body is not None:
+            return self.displayer(self.body)
+
+    def display(self, **kwargs):
+        object = self.render(**kwargs)
+        if object is None: return
+        if self.vars:
+            if self._display is None:
+                self._display = IPython.display.display(object, display_id=True)
+            else:
+                self._display.display(object)
+        else:
+            IPython.display.display(object)
+
+    _ipython_display_ = display
+
+class MarkdownDisplay(Display):
+    displayer = IPython.display.Markdown        
