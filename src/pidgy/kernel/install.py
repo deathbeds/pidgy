8d199c65e8b287ea9a9169f2a8f614a8c44beea3
tonyfast <tony.fast@gmail.com>
2021-11-13 18:57:22 -0800

    update kernel names from transition

--- src/pidgy/kernel/install.py
+++ src/pidgy/kernel/install.py
@@ -23,13 +23,13 @@ def install_jupyter_hook():
     user = "--user" in argv
     print("Installing Jupyter kernel spec:")
     try:
-        KernelSpecManager().install_kernel_spec(str(KERNELSPEC), "midgy", user=user)
+        KernelSpecManager().install_kernel_spec(str(KERNELSPEC), "pidgy", user=user)
         return
     except:
         pass
 
     try:
-        KernelSpecManager().install_kernel_spec(str(KERNELSPEC), "midgy", user=not user)
+        KernelSpecManager().install_kernel_spec(str(KERNELSPEC), "pidgy", user=not user)
         return
     except:
         pass
