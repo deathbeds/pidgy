d5e5cb4491719e1ec7adb1ebe05a313c95361fca
tonyfast <tony.fast@gmail.com>
2021-11-13 18:34:05 -0800

    nbval for tests

--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,21 @@
 from nox import session
 from nox.sessions import _SessionQuit
+from os import getenv
+
+CI = bool(getenv("CI"))
 
 
 @session(reuse_venv=True)
 def docs(session):
     session.install("jupyter-book", "sphinx-autoapi", "-e.")
     session.run("jb", "build", ".")
+
+
+@session(reuse_venv=not CI, python=False)
+def test(session):
+    session.install(CI and ".[test]" or "-e.[test]")
+    session.run("python", "-m", "pidgy.kernel.install")
+    session.run("pytest", "--nbval", *session.posargs)
 
 
 @session(reuse_venv=True)
