22a02c2b603fa715a83db57891a3ffce740229aa
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-12-03 22:12:48 -0600

    more work on dodo

--- dodo.py
+++ dodo.py
@@ -2,14 +2,96 @@
 from inspect import getsource
 from pathlib import Path
 from re import sub
+import shutil
 from textwrap import dedent
+from hashlib import sha256
+import sys
 
-from doit.tools import config_changed
+import doit.tools
 
-CONF = Path("conf.py")
+PY = Path(sys.executable)
 DOIT_CONFIG = dict(verbosity=2)
+HERE = Path(__file__).parent
+CONF = HERE / "conf.py"
+DIST = HERE / "dist"
+DOCS = HERE / "docs"
+LITE = HERE / "lite"
+SHA256SUMS = DIST / "SHA256SUMS"
+WHL_DEPS = [
+    "pyproject.toml",
+    "readme.md",
+    "setup.cfg",
+    "setup.py",
+    *Path("src").rglob("*.py"),
+    *Path("src/pidgy/kernel/pidgy").rglob("*"),
+]
 
 
+def task_dist():
+    """build distributions"""
+
+    def hashfile():
+        lines = []
+
+        for p in sorted([*DIST.glob("pidgy*.whl"), *DIST.glob("pidgy*.tar.gz")]):
+            if p == SHA256SUMS:
+                continue
+            lines += ["  ".join([sha256(p.read_bytes()).hexdigest(), p.name])]
+
+        output = "\n".join(lines)
+        print(output)
+        SHA256SUMS.write_text(output)
+
+    yield dict(
+        name="pidgy",
+        doc="build pidgy distributions",
+        file_dep=WHL_DEPS,
+        actions=[
+            lambda: [shutil.rmtree(DIST) if DIST.is_dir() else None, None][-1],
+            [PY, "setup.py", "sdist"],
+            [PY, "-m", "pip", "wheel", "-w", DIST, "--no-deps", "."],
+            hashfile,
+        ],
+        targets=[SHA256SUMS],
+    )
+
+
+@doit.create_after("dist")
+def task_lite():
+    """build jupyterlite site and pre-requisites"""
+    wheel = sorted(DIST.glob("pidgy*.whl"))[-1]
+
+    yield dict(
+        name="wheels",
+        file_dep=[SHA256SUMS, wheel],
+        actions=[
+            (doit.tools.create_folder, [LITE / "pypi"]),
+            doit.tools.CmdAction(
+                [PY, "-m", "pip", "download", "--prefer-binary", wheel],
+                cwd=str(LITE / "pypi"),
+                shell=False,
+            ),
+        ],
+    )
+
+    yield dict(
+        name="build",
+        file_dep=[
+            SHA256SUMS,
+            wheel,
+            LITE / "jupyter_lite_config.json",
+            *DOCS.rglob("*.ipynb"),
+        ],
+        actions=[
+            doit.tools.CmdAction(
+                [PY, "-m", "jupyter", "lite", "archive"], cwd=str(LITE), shell=False
+            )
+        ],
+        targets=[LITE / "output/SHA256SUMS"],
+    )
+
+
+@doit.create_after("lite")
 def task_docs():
     def write_addon():
         def setup(app):
