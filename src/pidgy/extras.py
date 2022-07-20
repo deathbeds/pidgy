9eda6fcd7b270c6ff15f5e5dcb43944f1164f0ef
Tony Fast <tony.fast@gmail.com>
2022-07-19 22:43:59 -0700

    Add IPython display objects to namespace along with return statement (#71)
    * add ipython display objects to the user namespace for convenience
    
    * add the return statement convenience
    
    * add docstrings

--- src/pidgy/extras.py
+++ src/pidgy/extras.py
@@ -6,9 +6,13 @@
   this feature minimizes time between computing iterations by bringing more completion sooner.
   in an interactive computing session, the sys modules hold variable names not available to
   the author, but when available help continue flow between executions.
+
+3. IPython display objects are added to the user namespace
+4. top-level return statement become IPython.display expressions.
 """
 
 from sys import modules
+from ast import NodeTransformer, Call, Expr, parse, Tuple
 import IPython
 
 
@@ -22,10 +26,52 @@ def update_sys_modules():
     )
 
 
+def ipython_display_objects(**data):
+    """extract the display object from IPython"""
+    from IPython import display
+    from IPython.display import DisplayObject, TextDisplayObject, IFrame
+
+    for k, v in vars(display).items():
+        if isinstance(v, type) and issubclass(v, (DisplayObject, IFrame)):
+            if v not in {DisplayObject, TextDisplayObject}:
+                if k[0].isupper():
+                    data[k] = v
+    return data
+
+
+class ReturnDisplay(NodeTransformer):
+    """transform a return node into an IPython display expression"""
+
+    def visit_FunctionDef(self, node):
+        return node
+
+    visit_AsyncFunctionDef = visit_FunctionDef
+
+    def visit_Return(self, node):
+        return Expr(
+            Call(
+                func=parse('__import__("IPython").display.display', mode="eval").body,
+                args=node.value.elts if isinstance(node.value, Tuple) else [node.value],
+                keywords=[],
+            )
+        )
+
+
 def load_ipython_extension(shell: IPython.InteractiveShell):
     shell.user_ns.setdefault("shell", shell)
+    shell.user_ns.update(
+        (k, v) for k, v in ipython_display_objects().items() if k not in shell.user_ns
+    )
     shell.events.register("pre_execute", update_sys_modules)
+    shell.ast_transformers.append(ReturnDisplay())
 
 
 def unload_ipython_extension(shell: IPython.InteractiveShell):
     shell.events.unregister("pre_execute", update_sys_modules)
+
+    pops = []
+    for i, x in enumerate(shell.ast_transformers):
+        isinstance(x, ReturnDisplay) and pops.append(i)
+
+    for pop in pops:
+        shell.ast_transformers.pop(pop)
