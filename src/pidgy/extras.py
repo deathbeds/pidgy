5e5a4159b97cd49fc57677c20359e8f8a3abae12
Tony Fast <tony.fast@gmail.com>
2022-08-03 22:27:52 -0700

    A extra magic features: json parsing and shebangs (#72)
    * add builtins for json copy pasta
    
    * include the shebang transformer.
    
    * introduce a cls for generically registering ipython extensions using naming conventions.
    
    * add test and documentation for extras
    
    * add to test framework, use magics
    
    * iterate on shebang
    
    * add to existing filter set if nbconvert is available

--- src/pidgy/extras.py
+++ src/pidgy/extras.py
@@ -9,38 +9,82 @@
 
 3. IPython display objects are added to the user namespace
 4. top-level return statement become IPython.display expressions.
+5. it should be easy to copy and paste json for beginners. 
+    we are `true, false and null` buultins.
 """
 
+from pathlib import Path
+from subprocess import CalledProcessError
 from sys import modules
 from ast import NodeTransformer, Call, Expr, parse, Tuple
 import IPython
+from traitlets import CUnicode
+from .pidgy import Extension
 
 
-def update_sys_modules():
-    """update the shell's user namespace to include imported modules"""
-    shell = IPython.get_ipython()
-    shell.user_ns.update(
-        (k, v)
-        for k, v in modules.items()
-        if k and k[0] != "_" and "." not in k and k not in shell.user_ns
-    )
+class SysModules(Extension):
+    def pre_execute(self):
+        """update the shell's user namespace to include imported modules"""
+        self.shell.user_ns.update(
+            (k, v)
+            for k, v in modules.items()
+            if k and k[0] != "_" and "." not in k and k not in self.shell.user_ns
+        )
 
 
-def ipython_display_objects(**data):
-    """extract the display object from IPython"""
-    from IPython import display
-    from IPython.display import DisplayObject, TextDisplayObject, IFrame
+class JsonPositive(Extension):
+    def load_ipython_extension(self):
+        import builtins
 
-    for k, v in vars(display).items():
-        if isinstance(v, type) and issubclass(v, (DisplayObject, IFrame)):
-            if v not in {DisplayObject, TextDisplayObject}:
-                if k[0].isupper():
-                    data[k] = v
-    return data
+        builtins.true, builtins.false, builtins.null = True, False, None
 
 
-class ReturnDisplay(NodeTransformer):
+class IPythonDisplays(Extension):
+    def load_ipython_extension(self):
+        """extract the display object from IPython"""
+        from IPython import display
+        from IPython.display import DisplayObject, TextDisplayObject, IFrame
+
+        for k, v in vars(display).items():
+            if isinstance(v, type) and issubclass(v, (DisplayObject, IFrame)):
+                if v not in {DisplayObject, TextDisplayObject}:
+                    if k[0].isupper():
+                        self.shell.user_ns.setdefault(k, v)
+
+
+class Shebang(Extension):
+    alias = CUnicode("/usr/bin/env")
+
+    def cleanup_transforms(self, lines):
+        for i, line in enumerate(map(str.strip, lines)):
+            if line:
+                if line.startswith(("#!",)):
+                    lines[i] = lines[0].replace("#!", "%%", 1)
+                break
+        return lines
+
+    def cell(self, argv, body):
+        import tempfile, subprocess, shlex
+        from IPython.display import Pretty, display
+
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".py") as file:
+            file.write(body.encode())
+        try:
+            output = subprocess.check_output(shlex.split(argv) + [file.name]).decode()
+            display(Pretty(output))
+        except CalledProcessError:
+            pass
+        finally:
+            Path(file.name).unlink()
+
+
+class ReturnDisplay(Extension, NodeTransformer):
     """transform a return node into an IPython display expression"""
+
+    REPLACEMENT = parse(
+        Extension.IS_IPY and '__import__("IPython").display.display' or "print",
+        mode="eval",
+    )
 
     def visit_FunctionDef(self, node):
         return node
@@ -50,7 +94,7 @@ class ReturnDisplay(NodeTransformer):
     def visit_Return(self, node):
         return Expr(
             Call(
-                func=parse('__import__("IPython").display.display', mode="eval").body,
+                func=self.REPLACEMENT.body,
                 args=node.value.elts if isinstance(node.value, Tuple) else [node.value],
                 keywords=[],
             )
@@ -59,19 +103,16 @@ class ReturnDisplay(NodeTransformer):
 
 def load_ipython_extension(shell: IPython.InteractiveShell):
     shell.user_ns.setdefault("shell", shell)
-    shell.user_ns.update(
-        (k, v) for k, v in ipython_display_objects().items() if k not in shell.user_ns
-    )
-    shell.events.register("pre_execute", update_sys_modules)
-    shell.ast_transformers.append(ReturnDisplay())
+    SysModules(shell=shell).load_ipython_extension()
+    IPythonDisplays(shell=shell).load_ipython_extension()
+    ReturnDisplay(shell=shell).load_ipython_extension()
+    JsonPositive(shell=shell).load_ipython_extension()
+    Shebang(shell=shell).load_ipython_extension()
 
 
 def unload_ipython_extension(shell: IPython.InteractiveShell):
-    shell.events.unregister("pre_execute", update_sys_modules)
-
-    pops = []
-    for i, x in enumerate(shell.ast_transformers):
-        isinstance(x, ReturnDisplay) and pops.append(i)
-
-    for pop in pops:
-        shell.ast_transformers.pop(pop)
+    SysModules(shell=shell).unload_ipython_extension()
+    IPythonDisplays(shell=shell).unload_ipython_extension()
+    ReturnDisplay(shell=shell).unload_ipython_extension()
+    JsonPositive(shell=shell).unload_ipython_extension()
+    Shebang(shell=shell).unload_ipython_extension()
