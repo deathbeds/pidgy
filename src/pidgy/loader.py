793e4eac238f2b947f3fe9a3006cfa03923e5e91
tonyfast <tony.fast@gmail.com>
2022-10-23 19:47:33 -0700

    update loader to what midgy does

--- src/pidgy/loader.py
+++ src/pidgy/loader.py
@@ -1,26 +1,14 @@
-from importnb import Notebook
+from midgy.loader import Markdown
 
 from .extras import ReturnDisplay
-from .markdown import Renderer
-from .tangle import Python
+from .tangle import IPython
 
 
-class Markdown(Notebook):
+class Pidgy(Markdown):
     """an importnb extension for pidgy documents"""
 
     extensions = ".py.md", ".md", ".md.ipynb"
-    tangle = Renderer(renderer_cls=Python)
-
-    def get_data(self, path):
-        if self.path.endswith(".md"):
-            self.source = self.decode()
-            return self.code(self.source)
-        return super(Notebook, self).get_data(path)
-
-    def code(self, str):
-        return super().code(self.tangle.render("".join(str)))
+    render_cls = IPython
 
     def visit(self, node):
         return super().visit(ReturnDisplay().visit(node))
-
-    get_source = get_data = get_data
