23fa9e0c5270da432514052b64aaef323287e400
tonyfast <tony.fast@gmail.com>
2023-01-03 20:51:37 -0800

    add filters

--- src/pidgy/filters.py
+++ src/pidgy/filters.py
@@ -1,6 +1,10 @@
 "jinja2 filters that make writing pidgy more fun"
 
-
+from . import get_ipython
 def q(q, cite):
     """a filter for <q>uotes"""
     return f"""<q cite="{cite}">{q}</q>"""
+
+
+def md(body):
+    return get_ipython().weave.markdown_renderer.render(body)
