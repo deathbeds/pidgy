06da74472f234b44efab634819f8dbf38aa5a700
tonyfast <tony.fast@gmail.com>
2023-02-09 13:50:13 -0800

    add i frame and quote filters

--- src/pidgy/filters.py
+++ src/pidgy/filters.py
@@ -2,9 +2,21 @@
 
 from . import get_ipython
 def q(q, cite):
-    """a filter for <q>uotes"""
+    """<q>uotes"""
     return f"""<q cite="{cite}">{q}</q>"""
 
+def i(id, height=600, width="100%"):
+    """a filter for <q>uotes"""
+    from urllib.parse import urlparse
+    from html import escape
+
+    parse = urlparse(id)
+
+    src = "src"
+    if parse.scheme and parse.netloc:
+        id = escape(id)
+        src += "doc"
+    return f"""<iframe {src}="{id}" width="{width}" height="{height}"/>"""
 
 def md(body):
     return get_ipython().weave.markdown_renderer.render(body)
