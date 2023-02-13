1297b6b1acc25fb06ead062127c633e630580496
tonyfast <tony.fast@gmail.com>
2023-02-13 13:36:12 -0800

    copy logo not move

--- docs/hooks.py
+++ docs/hooks.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import logging
 from os import environ
-from shutil import move
+from shutil import copy
 
 ROOT = Path(__file__).parent.parent
 LOGO = ROOT / "pidgy.png"
@@ -14,7 +14,7 @@ def on_pre_build(config):
 
     if not (DOCS / LOGO.name).exists():
         log.info("move logo to docs dir")
-        move(LOGO, DOCS)
+        copy(LOGO, DOCS)
 
 
 def on_post_build(config):
