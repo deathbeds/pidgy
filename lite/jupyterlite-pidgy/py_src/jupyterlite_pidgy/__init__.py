369802be68773351575c0c8d088cf31a748ce1d0
tonyfast <tony.fast@gmail.com>
2023-02-13 13:16:41 -0800

    rm the old lite kernel and prefer the magics

--- lite/jupyterlite-pidgy/py_src/jupyterlite_pidgy/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._version import __version__, __js__
-
-def _jupyter_labextension_paths():
-    return [{ "src": "labextension", "dest": __js__["name"]}]
--- lite/jupyterlite-pidgy/py_src/jupyterlite_pidgy/_version.py
+++
@@ -1,8 +0,0 @@
-import json
-from pathlib import Path
-
-__all__ = ["__version__", "__js__"]
-__js__ = json.load(
-    (Path(__file__).parent.resolve() / "labextension/package.json").read_bytes()
-)
-__version__ = __js__["version"]
