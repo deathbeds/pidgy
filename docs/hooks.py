9ea1027ff3ddbf510dadc626aa13799b846c8032
tonyfast <tony.fast@gmail.com>
2023-02-21 15:50:51 -0800

    update hooks implementation to create lite on readthedocs

--- docs/hooks.py
+++ docs/hooks.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import logging
-from os import environ
+from os import environ, system
+from subprocess import check_output
 from shutil import copy
 
 ROOT = Path(__file__).parent.parent
@@ -11,14 +12,15 @@ log = logging.getLogger("mkdocs")
 
 
 def on_pre_build(config):
-
     if not (DOCS / LOGO.name).exists():
         log.info("move logo to docs dir")
         copy(LOGO, DOCS)
 
 
 def on_post_build(config):
-    pass
+    if "READTHEDOCS" in environ:
+        log.info("building lite")
+        log.info(check_output(["doit", "lite"], cwd=ROOT)        )
 
 
 # https://www.mkdocs.org/user-guide/configuration/#hooks
