7234fc1da34a3ef03a05c401a83e0aa0e38c9f8f
tonyfast <tony.fast@gmail.com>
2023-02-21 14:55:12 -0800

    add back old lite configs

---
+++ lite/jupyterlite-pidgy/src/tokens.ts
@@ -0,0 +1,19 @@
+export * from './_pypi';
+
+/**
+ * The default CDN fallback for Pyodide
+ */
+export const PYODIDE_CDN_URL =
+  'https://cdn.jsdelivr.net/pyodide/v0.19.0/full/pyodide.js';
+
+/**
+ * The id for the upstream extension, and key in the litePlugins.
+ */
+
+export const UPSTREAM_PLUGIN_ID = '@jupyterlite/pyolite-kernel-extension:kernel';
+
+/**
+ * The id for the extension.
+ */
+
+export const PLUGIN_ID = '@deathbeds/pidgy:kernel';
